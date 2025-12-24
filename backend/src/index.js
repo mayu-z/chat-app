d9546ba911d971a46662fc1c96717c3228ca1e03
may-z <mayureshsingh338@gmail.com>
2025-12-24 21:30:47 +0530

    Initialize  Docker Compose and Nginx proxy.

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
